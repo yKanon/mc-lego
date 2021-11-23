5e10f387dd0c4c41c5ddb4ab68c7842012a682f7
文三思 <394769450@qq.com>
2021-11-23 13:05:14 +0800

    check node version

--- lego-cli/core/cli/lib/index.js
+++ lego-cli/core/cli/lib/index.js
@@ -1,14 +1,28 @@
 'use strict';
 
-const pkg = require('../package.json');
+const semver = require('semver')
 const log = require('@wss-/log')
+
+const pkg = require('../package.json');
+const { LOWEST_NODE_VERSION} = require('./const')
 
 module.exports = core;
 
 function core() {
+  checkNodeVersion()
   checkPackageVersion()
 }
 
 function checkPackageVersion() {
-  log.notice('cli  version', pkg.version);
+  log.notice('package version', pkg.version)
+}
+
+function checkNodeVersion() {
+  const currentNodeVersion = process.version
+  const lowestNodeVersion = LOWEST_NODE_VERSION
+
+  if (semver.lte(currentNodeVersion, lowestNodeVersion)) {
+    log.error(`Your node version is ${currentNodeVersion}, but the minimum required version is ${lowestNodeVersion}`)
+    process.exit(1)
+  }
 }
