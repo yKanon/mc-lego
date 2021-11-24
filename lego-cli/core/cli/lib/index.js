cf51b99721e8431e61f12eb7b54da3011829d8f4
文三思 <394769450@qq.com>
2021-11-24 10:27:45 +0800

    check userHome

--- lego-cli/core/cli/lib/index.js
+++ lego-cli/core/cli/lib/index.js
@@ -2,28 +2,43 @@
 
 import semver from 'semver'
 import log from '@wss-/log'
+import { existsSync } from  'fs'
+import colors from 'colors/safe.js'
 
 import pkg from '../package.json'
 import { LOWEST_NODE_VERSION } from './const.js'
 
-function core() {
-  checkPackageVersion()
-  checkNodeVersion()
-  checkRoot()
+async function core() {
+  try {
+    checkPackageVersion()
+    checkNodeVersion()
+    await checkRoot()
+    await checkUserHome()
+  } catch (e) {
+    log.error(colors.red(e.message))
+  }
 }
 
 async function checkRoot() {
   // const rootCheck = (await import('root-check')).default
   const { default:rootCheck } = await import('root-check')
   rootCheck()
-  console.log(process.geteuid());
   // import('root-check').then(({ default: rootCheck }) => {
   //   rootCheck()
   // })
 }
 
+async function checkUserHome() {
+  const { homedir } = await import('os')
+  const userHome = homedir()
+
+  if (!userHome || existsSync(userHome)) {
+    throw new Error(`当前用户主目录不存在，请检查系统配置。`)
+  }
+}
+
 function checkPackageVersion() {
-  log.notice('package version', pkg.version)
+  log.info('cli', pkg.version)
 }
 
 function checkNodeVersion() {
@@ -31,10 +46,8 @@ function checkNodeVersion() {
   const lowestNodeVersion = LOWEST_NODE_VERSION
 
   if (semver.lte(currentNodeVersion, lowestNodeVersion)) {
-    log.error(`Your node version is ${currentNodeVersion}, but the minimum required version is ${lowestNodeVersion}`)
-    process.exit(1)
+    throw new Error(`当前 Node 版本 ${currentNodeVersion} 不支持，请升级 Node 至 ${lowestNodeVersion} 以上版本。`)
   }
 }
 
-// console.log(core);
 export default core;
