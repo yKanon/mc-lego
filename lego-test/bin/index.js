3ffc30152fcdb4377ac7dab89886ce1d36ca1c94
yKanon <394769450@qq.com>
2022-01-08 23:38:54 +0800

    feat: commander base usage

--- lego-test/bin/index.js
+++ lego-test/bin/index.js
@@ -9,4 +9,14 @@ const program = new commander.Command();
 
 const pkg = require('../package.json');
 
-program.version(pkg.version).parse(process.argv);
+program
+  .name(Object.keys(pkg?.bin)[0])
+  .usage('[global options] command')
+  .version(pkg.version)
+  .option('-d, --debug', '是否开启调试模式', false)
+  .option('-e, --envName <envName>', '获取环境变量名称')
+  .parse(process.argv);
+
+  console.log(program.debug)
+  console.log(program.opts());
+  program.outputHelp()
